0b925d2c39c9d9eea6757e91562178706561f9a2
Demetrio Guilardi <deguilardi@Demetrios-Mac-Pro.local>
2018-01-04 00:06:00 -0500

    cleaning

--- cocos/platform/android/libcocos2dx/build/generated/source/aidl/debug/com/enhance/gameservice/IGameTuningService.java
+++ cocos/platform/android/libcocos2dx/build/generated/source/aidl/debug/com/enhance/gameservice/IGameTuningService.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: /Users/deguilardi/cocosWorkspace/eu-sei/cocos2d/cocos/platform/android/java/src/com/enhance/gameservice/IGameTuningService.aidl
+ * Original file: /Users/deguilardi/cocosWorkspace/lib-cocos2d/cocos/platform/android/java/src/com/enhance/gameservice/IGameTuningService.aidl
  */
 package com.enhance.gameservice;
 public interface IGameTuningService extends android.os.IInterface
